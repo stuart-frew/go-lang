2668079507dff62cc58402753838dff254410eee
Stuart <stuart@frew.net.nz>
2022-01-27 17:12:39 +1300

    Extend api

--- src/main.go
+++ src/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title  string `json:"Title"`
+	Author string `json:"author"`
+	Link   string `json:"link"`
+}
+
+var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -13,9 +22,27 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func main() {
+	Articles = []Article{
+		{Title: "Python Intermediate and Advanced 101",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089KVK23P"},
+		{Title: "R programming Advanced",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089WH12CR"},
+		{Title: "R programming Fundamentals",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089S58WWG"},
+	}
+
 	handleRequests()
 }
